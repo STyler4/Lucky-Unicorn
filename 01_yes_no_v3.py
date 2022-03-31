1e74138f45c5ba501a8cb1d69d7551278d2c619e
STyler4 <98915469+STyler4@users.noreply.github.com>
2022-03-31 18:13:14 +1300

    Update 01_yes_no_v3.py

--- 01_yes_no_v3.py
+++ 01_yes_no_v3.py
@@ -3,20 +3,22 @@ Simplifies the input by converting it to lower case. Also, accepts y or n as
 abbreviations. Prints result of user choice as well as input - for testing
 """
 
-# Ask the user if they have played before
-show_instructions = input("have you played this game before?: ").lower()
+show_instructions = ""
+while show_instructions != "x":
+    # Ask the user if they have played before
+    show_instructions = input("have you played this game before?: ").lower()
 
-# If they say yes, output 'Program Continues'
-if show_instructions == "yes" or show_instructions == "y":
-    print("program continues")
+    # If they say yes, output 'Program Continues'
+    if show_instructions == "yes" or show_instructions == "y":
+        print("program continues")
 
-# If they say no, output 'Display Instructions'
-elif show_instructions == "no" or show_instructions == "n":
-    print("Display instructions")
+    # If they say no, output 'Display Instructions'
+    elif show_instructions == "no" or show_instructions == "n":
+        print("Display instructions")
 
-# Otherwise - show error
-else:
-    print("Please answer 'yes or 'no': ")
+    # Otherwise - show error
+    else:
+        print("Please answer 'yes or 'no': ")
 
 
 print(f"You entered '{show_instructions}'")
