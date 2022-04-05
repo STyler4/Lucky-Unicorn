5527bda8371b8a775169b4021062131bdc611fff
STyler4 <98915469+STyler4@users.noreply.github.com>
2022-04-05 15:13:29 +1200

    04_how_much.py

---
+++ 04_how_much_v4.py
@@ -0,0 +1,8 @@
+"""Component 2 (How much) v4
+Changing v3 into a function
+Also needed to change user_balance to the more generic variable name 'response'
+and to change the condition and position of the number comparison into the loop
+to make the function recyclable"""
+
+
+
