c84853d61502015c0264ad304098b30f6029b069
STyler4 <98915469+STyler4@users.noreply.github.com>
2022-04-06 10:15:56 +1200

    How_much

--- 04_how_much_v4.py
+++ 04_how_much_v4.py
@@ -5,4 +5,26 @@ and to change the condition and position of the number comparison into the loop
 to make the function recyclable"""
 
 
+def num_check(question, low, high):
+    error = "That was not a valid input\n" \
+            "Please enter a number between {} and {}\n".format(low, high)
 
+    # Keep asking until a valid amount (1-10) is entered
+    while True:
+        try:
+            # ask for amount
+            response = int(input(question))
+
+            # check for number within the required range
+            if low <= response <= high:
+                return response
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+
+# Main routine
+user_balance = num_check("How much would you like to play with? $", 1, 10)
+print(f"You are playing with ${user_balance}")
